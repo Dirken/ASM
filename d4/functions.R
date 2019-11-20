655e18f153cc52bd693a4985970868d61548785d
RMeyerhofer <ricard.meyerhofer@est.fib.upc.edu>
2019-11-20 22:33:08 +0100

    done

--- d4/functions.R
+++ d4/functions.R
@@ -1,19 +1,14 @@
 MSPEkfold <- function(X, Y, K, n.lambdas, lambda.v, n) {
   PMSE.CV <-matrix(nrow = K, ncol=n.lambdas)
   folds <- sample(rep(1:K, length=n), n, replace=FALSE) 
-  
   MPSE <- data.frame()
-  
   # Iterate through K Folds
   for (k in 1:K){
     Xk <- as.matrix(X[folds != k,])
     Yk <- as.matrix(Y[folds != k])
-    
     Xv <- as.matrix(X[folds == k,])
     Yv <- as.matrix(Y[folds == k])
-    
     r <- dim(Xv)[1]
-    
     mspe.k.val = MSPEvalidation(Xk, Yk, Xv, Yv, lambda.v, n.lambdas)
     MPSE <- rbind(MPSE, mspe.k.val) 
   }
@@ -24,8 +19,6 @@ MSPEkfold <- function(X, Y, K, n.lambdas, lambda.v, n) {
   return(MPSE2)
 }
 
-
-
 # leave-one-out
 MSPEcv <- function(X, Y, n.lambdas, lambda.v, n) {
   PMSE.CV <- numeric(n.lambdas)
@@ -35,8 +28,10 @@ MSPEcv <- function(X, Y, n.lambdas, lambda.v, n) {
     for (i in 1:n){
       #   m.Y.i <- mean(Y[-i])
       m.Y.i <- 0
-      X.i <- X[-i,]; Y.i <- Y[-i]-m.Y.i
-      Xi <- X[i,]; Yi <- Y[i]
+      X.i <- X[-i,]
+      Y.i <- Y[-i]-m.Y.i
+      Xi <- X[i,]
+      Yi <- Y[i]
       beta.i <- solve(t(X.i)%*%X.i + lambda*diag(1,p)) %*% t(X.i) %*% Y.i
       hat.Yi <- Xi %*% beta.i + m.Y.i
       PMSE.CV[l] <- PMSE.CV[l] + (hat.Yi-Yi)^2
@@ -48,13 +43,11 @@ MSPEcv <- function(X, Y, n.lambdas, lambda.v, n) {
 
 MSPEvalidation <- function(X, Y, Xval, Yval, lambda.v, n.lambdas) {
   PMSE.VAL <- n.lambdas
-  
   # Iterate through candidate values
   for (l in 1:n.lambdas){
     lambda <- lambda.v[l]
     r <- dim(Xval)[1]
     PMSE.VAL[l] <- 0
-    
     # Compute beta with the traning dataset
     beta.i <- solve(t(X)%*%X + lambda*diag(1,p)) %*% t(X) %*% Y
     for (i in 1:r){
